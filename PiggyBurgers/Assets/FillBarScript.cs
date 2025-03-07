3ef3f1ce4ebdf383cff7449e6fc942d76589c655
Lyubomir Kolev <lubokolev14@gmail.com>
2025-03-07 16:21:58 +0100

    SliderWorking

--- PiggyBurgers/Assets/FillBarScript.cs
+++ PiggyBurgers/Assets/FillBarScript.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FillBarScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Image fillbar; // The UI Image to update the fill
+    public BellyExploding bellyExploding;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get the totalScore from BellyExploding
+        float totalScore = bellyExploding.totalScore;
+
+        // Normalize the totalScore to a value between 0 and 1
+        // First, make sure the score is clamped between 68 and 130
+        float normalizedScore = Mathf.Clamp01((totalScore - 68f) / (130f - 68f));
+
+        // Update the fill amount of the slider (or fillbar)
+        fillbar.fillAmount = normalizedScore;
     }
 }
