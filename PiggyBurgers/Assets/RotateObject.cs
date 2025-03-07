6e5d06855e09147f625af63863b737180a9bd2cc
Magdalena Nikolova <maddynikolova@gmail.com>
2025-03-07 15:00:01 +0100

    RotateObjects

--- PiggyBurgers/Assets/RotateObject.cs
+++ PiggyBurgers/Assets/RotateObject.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class RotateObject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed = 50f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(1, 1, 1) * rotationSpeed * Time.deltaTime);
     }
 }
